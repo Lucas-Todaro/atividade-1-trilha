38e1531816d113b1c0d65373c8dc0bf45c5951f0
Lucas-Todaro <ltodarosouza@gmail.com>
2026-07-01 14:45:49 -0300

    Terceira atividade

---
+++ aula3/calculadora.py
@@ -0,0 +1,7 @@
+def somar(a, b):
+    return a + b
+def multiplicar(a, b):
+    return a*b
+if __name__ == '__main__':
+    print(somar(3,5))
+    print(multiplicar(3, 5))
---
+++ aula3/main.py
@@ -0,0 +1,4 @@
+import calculadora
+
+print(calculadora.somar(3, 10))
+print(calculadora.multiplicar(4, 3))
